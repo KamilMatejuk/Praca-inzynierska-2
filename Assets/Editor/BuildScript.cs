79ed2ed4290ce66c98833c70da28d8ae46c7f4ae
kamil <kamilmatejuk@gmail.com>
2022-01-31 00:17:57 +0100

    build documnetation

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,29 +1,42 @@
 using UnityEditor;
 using UnityEngine;
 
+namespace RacingGameBot.Editors {
+    public class BuildScript {
 
-public class BuildScript {
+        [SerializeField, HideInInspector] private static string gameName = "Gra";
+        [SerializeField, HideInInspector] private static string[] levels = new string[] { "Assets/Scenes/Main Menu.unity", "Assets/Scenes/Create Level.unity", "Assets/Scenes/Game.unity" };
 
-    [SerializeField, HideInInspector] private static string gameName = "Gra";
-    [SerializeField, HideInInspector] private static string[] levels = new string[] { "Assets/Scenes/Main Menu.unity", "Assets/Scenes/Create Level.unity", "Assets/Scenes/Game.unity" };
+        /// <summary>
+        /// Build game executable for Windows
+        /// </summary>
+        [MenuItem("MyTools/Custom Windows Build")]
+        public static void BuildGameWin() => BuildGame(BuildTarget.StandaloneWindows64, "exe", new string[] { "unity_builtin_extra", "unity default resources" });
 
-    [MenuItem("MyTools/Custom Windows Build")]
-    public static void BuildGameWin() => BuildGame(BuildTarget.StandaloneWindows64, "exe", new string[] { "unity_builtin_extra", "unity default resources" });
+        /// <summary>
+        /// Build game executable for Linux
+        /// </summary>
+        [MenuItem("MyTools/Custom Linux Build")]
+        public static void BuildGameLinux() => BuildGame(BuildTarget.StandaloneLinux64, "x86_64", new string[] { "unity_builtin_extra", "unity default resources", "UnityPlayer.png" });
 
-    [MenuItem("MyTools/Custom Linux Build")]
-    public static void BuildGameLinux() => BuildGame(BuildTarget.StandaloneLinux64, "x86_64", new string[] { "unity_builtin_extra", "unity default resources", "UnityPlayer.png" });
-
-    private static void BuildGame(BuildTarget system, string extension, string[] files) {
-        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
-        if (path.Length > 0) {
-            BuildPipeline.BuildPlayer(levels, $"{path}/{gameName}.{extension}", system, BuildOptions.None);
-            FileUtil.CopyFileOrDirectory($"{path}/{gameName}_Data/Resources/", $"{path}/{gameName}_Data/Temp_Resources/");
-            FileUtil.DeleteFileOrDirectory($"{path}/{gameName}_Data/Resources/");
-            FileUtil.CopyFileOrDirectory("Assets/Resources/", $"{path}/{gameName}_Data/Resources/");
-            foreach (string file in files) {
-                FileUtil.CopyFileOrDirectory($"{path}/{gameName}_Data/Temp_Resources/{file}", $"{path}/{gameName}_Data/Resources/{file}");
+        /// <summary>
+        /// Builder for game executables based on system
+        /// </summary>
+        /// <param name="system">target system</param>
+        /// <param name="extension">game file extension</param>
+        /// <param name="files">list of </param>
+        private static void BuildGame(BuildTarget system, string extension, string[] files) {
+            string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+            if (path.Length > 0) {
+                BuildPipeline.BuildPlayer(levels, $"{path}/{gameName}.{extension}", system, BuildOptions.None);
+                FileUtil.CopyFileOrDirectory($"{path}/{gameName}_Data/Resources/", $"{path}/{gameName}_Data/Temp_Resources/");
+                FileUtil.DeleteFileOrDirectory($"{path}/{gameName}_Data/Resources/");
+                FileUtil.CopyFileOrDirectory("Assets/Resources/", $"{path}/{gameName}_Data/Resources/");
+                foreach (string file in files) {
+                    FileUtil.CopyFileOrDirectory($"{path}/{gameName}_Data/Temp_Resources/{file}", $"{path}/{gameName}_Data/Resources/{file}");
+                }
+                FileUtil.DeleteFileOrDirectory($"{path}/{gameName}_Data/Temp_Resources/");
             }
-            FileUtil.DeleteFileOrDirectory($"{path}/{gameName}_Data/Temp_Resources/");
         }
     }
 }
