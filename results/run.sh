f73da04c7913932b27e8b81c22b4cd2600fcc269
kamil <kamilmatejuk@gmail.com>
2022-02-05 18:09:25 +0100

    results

--- results/run.sh
+++ results/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mlagents-learn results/configuration.yaml --run-id level1 --force --env results/envs/level1 --no-graphics
-mlagents-learn results/configuration.yaml --run-id level2 --force --env results/envs/level2 --no-graphics --initialize-from level1
-mlagents-learn results/configuration.yaml --run-id level3 --force --env results/envs/level3 --no-graphics --initialize-from level2
-mlagents-learn results/configuration.yaml --run-id level4 --force --env results/envs/level4 --no-graphics --initialize-from level3
-mlagents-learn results/configuration.yaml --run-id level5 --force --env results/envs/level5 --no-graphics --initialize-from level4
-mlagents-learn results/configuration.yaml --run-id level6 --force --env results/envs/level6 --no-graphics --initialize-from level5
+mlagents-learn results/configuration.yaml --run-id level6_3 --env results/envs/level6_3/Gra --no-graphics --initialize-from level5 --resume
+
+# mlagents-learn results/configuration.yaml --run-id level1 --force --env results/envs/level1 --no-graphics
+# mlagents-learn results/configuration.yaml --run-id level2 --force --env results/envs/level2 --no-graphics --initialize-from level1
+# mlagents-learn results/configuration.yaml --run-id level3 --force --env results/envs/level3 --no-graphics --initialize-from level2
+# mlagents-learn results/configuration.yaml --run-id level4 --force --env results/envs/level4 --no-graphics --initialize-from level3
+# mlagents-learn results/configuration.yaml --run-id level5 --force --env results/envs/level5 --no-graphics --initialize-from level4
+# mlagents-learn results/configuration.yaml --run-id level6 --force --env results/envs/level6 --no-graphics --initialize-from level5
